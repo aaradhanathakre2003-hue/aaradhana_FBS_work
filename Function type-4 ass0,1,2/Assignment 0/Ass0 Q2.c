458ab683941d1021c4a1ff39b8c2faf945ef3bf4
Aaradhana Sharad Thakre <aaradhanathakre2003@gmail.com>
2026-08-05 11:12:46 +0530

    Add files via upload

---
+++ Function type-4 ass0,1,2/Assignment 0/Ass0 Q2.c
@@ -0,0 +1,10 @@
+float areacircle(float,float);
+void main()
+{
+    float area=areacircle(3.14,10);
+    printf("Area = %.2f",area);
+}
+float areacirclefloat pi,float r()
+{
+    return pi*r*r;
+}
